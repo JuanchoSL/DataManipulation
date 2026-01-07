2220208082782a696a10681841b64ac1c64b226e
Juan Sánchez <JuanchoSL@hotmail.com>
2026-01-07 10:09:10 +0100

    array lists manipulators

---
+++ src/Manipulators/Arrays/ArrayManipulators.php
@@ -0,0 +1,79 @@
+<?php declare(strict_types=1);
+
+namespace JuanchoSL\DataManipulation\Manipulators\Arrays;
+
+use JuanchoSL\DataManipulation\Traits\CallfuncTrait;
+
+class ArrayManipulators
+{
+
+    use CallfuncTrait;
+
+    public function slice(int $offset, ?int $length = null, bool $preserve_keys = false): static
+    {
+        return $this->sanitize('array_slice', func_get_args());
+        return $this->sanitize('array_slice', ['merge' => func_get_args()]);
+    }
+
+    public function chunk(int $length, bool $preserve_keys = false): static
+    {
+        return $this->sanitize('array_chunk', func_get_args());
+        return $this->sanitize('array_chunk', ['merge' => func_get_args()]);
+    }
+
+    public function column(int|string|null $column_key, int|string|null $index_key = null): static
+    {
+        return $this->sanitize('array_column', func_get_args());
+        return $this->sanitize('array_column', ['merge' => func_get_args()]);
+    }
+
+    //@TODO hay que invertir keys y values
+    public function combine(array $keys): static
+    {
+        //$this->sanitize('array_combine', ['map' => $keys]);
+        $this->sanitize('array_combine', $keys);
+        return $this->flip();
+    }
+
+    public function countValuesRepetitions(): static
+    {
+        return $this->sanitize('array_count_values');
+    }
+
+    public function merge(): static
+    {
+        return $this->sanitize('array_merge');
+    }
+
+    public function fillByKey(array $keys, mixed $value): static
+    {
+        return $this->sanitize('array_fill_keys', func_get_args());
+    }
+
+    public function filter(): static
+    {
+        return $this->sanitize('array_filter');
+    }
+
+    public function flip(): static
+    {
+        return $this->sanitize('array_flip');
+        return $this->sanitize('array_flip', ['iterable' => true]);
+    }
+
+    public function unique(): static
+    {
+        return $this->sanitize('array_unique', ['iterable' => true]);
+        return $this->sanitize('array_unique');
+    }
+
+    public function sum(): static
+    {
+        return $this->sanitize('array_sum');
+    }
+    
+    public function product(): static
+    {
+        return $this->sanitize('array_product');
+    }
+}
---
+++ tests/Unit/ArrayManipulatorsTest.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace JuanchoSL\DataManipulation\Tests\Unit;
+
+use JuanchoSL\DataManipulation\Manipulators\Arrays\ArrayManipulators;
+use PHPUnit\Framework\TestCase;
+
+class ArrayManipulatorsTest extends TestCase
+{
+    public function testSlice()
+    {
+        $data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+        $tool = new ArrayManipulators();
+        $tool->slice(5);
+        $this->assertCount(5, $tool($data));
+    }
+    public function testChunk()
+    {
+        $data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+        $tool = new ArrayManipulators();
+        $tool->chunk(5);
+        $this->assertCount(2, $tool($data));
+
+    }
+
+    public function testCombine()
+    {
+        $this->markTestSkipped();
+        $keys = ['name', 'surname'];
+        $data = ['pepe', 'santos'];
+        //$data = [['pepe', 'santos'], ['manuel', 'dominguez'], ['ana', 'caballero']];
+
+        $tool = new ArrayManipulators();
+        $tool->combine($keys);
+        $results = $tool($data);
+        //echo "<pre>" . print_r($results, true);exit;
+
+        foreach ($results as $result) {
+            foreach ($keys as $key) {
+                $this->assertArrayHasKey($key, $result);
+            }
+        }
+    }
+
+    public function testCountValuesRepetitions()
+    {
+        $data = ['pepe', 'santos', 'manuel', 'dominguez', 'ana', 'caballero'];
+
+        $tool = new ArrayManipulators();
+        $tool->countValuesRepetitions();
+        $results = $tool($data);
+
+        $this->assertCount(count($data), $results);
+        foreach ($data as $key) {
+            $this->assertArrayHasKey($key, $results);
+            $this->assertIsInt($results[$key]);
+            $this->assertEquals(1, $results[$key]);
+        }
+    }
+
+    public function testMerge()
+    {
+        $data = [['name', 'surname'], ['name', 'surname'], ['name', 'surname'], ['name', 'surname']];
+
+        $tool = new ArrayManipulators();
+        $tool->merge();
+        $results = $tool(...$data);
+        $this->assertCount(8, $results);
+    }
+
+    public function testMergeAndUnique()
+    {
+        $data = [['name', 'surname'], ['name', 'surname'], ['name', 'surname'], ['name', 'surname']];
+
+        $tool = new ArrayManipulators();
+        $tool->merge()->unique();
+        $results = $tool(...$data);
+        $this->assertCount(2, $results);
+    }
+    /*    
+    public function testFillByKey()
+    {
+        $data = [['name', 'surname']];
+
+        $tool = new ArrayMapManipulators();
+        $tool->fill('');
+        $results = $tool(...$data);
+
+        foreach ($results as $result) {
+            foreach (['name', 'surname'] as $key) {
+                $this->assertArrayHasKey($key, $result);
+            }
+        }
+
+    }
+    public function testFilter()
+    {
+        $data = [['pepe' => 'name', 'santos' => 'surname'], ['manuel' => 'name', 'dominguez' => 'surname'], ['ana' => 'name', 'caballero' => 'surname']];
+
+        $tool = new ArrayManipulators();
+        $tool->flip();
+        $results = $tool(...$data);
+
+        foreach ($results as $result) {
+            foreach (['name', 'surname'] as $key) {
+                $this->assertArrayHasKey($key, $result);
+            }
+        }
+    }
+    */
+    public function testFlip()
+    {
+        $this->markTestSkipped();
+        $data = [['pepe' => 'name', 'santos' => 'surname'], ['manuel' => 'name', 'dominguez' => 'surname'], ['ana' => 'name', 'caballero' => 'surname']];
+
+        $tool = new ArrayManipulators();
+        $tool->flip();
+        $results = $tool(...$data);
+
+        //echo "<pre>" . print_r($results, true);exit;
+
+        foreach ($results as $result) {
+            foreach (['name', 'surname'] as $key) {
+                $this->assertArrayHasKey($key, $result);
+            }
+        }
+    }
+    public function testColumn()
+    {
+        $data = [['name' => 'pepe', 'surname' => 'santos'], ['name' => 'manuel', 'surname' => 'dominguez'], ['name' => 'ana', 'surname' => 'caballero']];
+
+        $tool = new ArrayManipulators();
+        $tool->column('surname', 'name');
+        $results = $tool($data);
+
+        foreach ($results as $result) {
+            $this->assertIsNotArray($result);
+        }
+    }
+    public function testUnique()
+    {
+
+        $data = ['pepe', 'santos', 'pepe', 'dominguez', 'ana', 'caballero'];
+
+        $tool = new ArrayManipulators();
+        $tool->unique();
+        $results = $tool($data);
+        $this->assertLessThan(count($data), count($results));
+    }
+
+    public function testSum()
+    {
+        $data = [1, 2, 3, 4];
+
+        $tool = new ArrayManipulators();
+        $tool->sum();
+        $results = $tool($data);
+        $this->assertEquals(10, $results);
+    }
+
+    public function testProd()
+    {
+        $data = [1, 2, 3, 4];
+
+        $tool = new ArrayManipulators();
+        $tool->product();
+        $results = $tool($data);
+        $this->assertEquals(24, $results);
+    }
+
+}

db311ea4002515a104f782bf042a547fc61d5a70
Juan Sánchez <JuanchoSL@hotmail.com>
2026-08-11 16:47:01 +0200

    update tests for new sanitizers

--- tests/Unit/Sanitizers/NumbersSanitizerTest.php
+++ tests/Unit/Sanitizers/NumbersSanitizerTest.php
@@ -9,47 +9,70 @@ class NumbersSanitizerTest extends TestCase
 {
     public function testIntegerTrue()
     {
-        $this->assertEquals(2000, (new NumberSanitizers())->integer()->__invoke('2000'));
-        $this->assertEquals(2000, (new NumberSanitizers())->integer()->__invoke('2000€'));
-        $this->assertEquals(2000, (new NumberSanitizers())->integer()->__invoke('2000.22'));
-        $this->assertEquals(2000, (new NumberSanitizers())->integer()->__invoke('2000.22€'));
-        $this->assertEquals(2000, (new NumberSanitizers())->integer()->__invoke('2,000.22€'));
+        $this->assertEquals(2000, (string) (new NumberSanitizers())->integer()->__invoke('2000€'));
+        $this->assertEquals(2000, (string) (new NumberSanitizers())->integer()->__invoke('2000.22'));
+        $this->assertEquals(2000, (string) (new NumberSanitizers())->integer()->__invoke('2000.22€'));
+        $this->assertEquals(2000, (string) (new NumberSanitizers())->integer()->__invoke('2,000.22€'));
+        $this->assertEquals(2000, (string) (new NumberSanitizers())->integer()->__invoke('2000'));
     }
 
     public function testIntegerCallableTrue()
     {
         $sanitizer = (new NumberSanitizers())->integer();
-        $this->assertEquals(2000, $sanitizer('2000'));
-        $this->assertEquals(2000, $sanitizer('2000€'));
-        $this->assertEquals(2000, $sanitizer('2000.22'));
-        $this->assertEquals(2000, $sanitizer('2000.22€'));
-        $this->assertEquals(2000, $sanitizer('2,000.22€'));
+        $this->assertEquals(2000, (string) $sanitizer('2000'));
+        $this->assertEquals(2000, (string) $sanitizer('2000€'));
+        $this->assertEquals(2000, (string) $sanitizer('2000.22'));
+        $this->assertEquals(2000, (string) $sanitizer('2000.22€'));
+        $this->assertEquals(2000, (string) $sanitizer('2,000.22€'));
     }
 
+    /*
+    public function testIntegerWithThousandsCallableTrue()
+    {
+        $sanitizer = (new NumberSanitizers())->integer(true);
+        //$this->assertEquals("2,000", $sanitizer('2000'));
+        //$this->assertEquals("2,000", $sanitizer('2000€'));
+        $this->assertEquals("2,000", $sanitizer('2000.22'));
+        $this->assertEquals("2,000", $sanitizer('2000.22€'));
+        $this->assertEquals("2,000", $sanitizer('2,000.22€'));
+    }
+*/
     public function testFloatTrue()
     {
-        $this->assertEquals(2000, (new NumberSanitizers())->float()->__invoke('2000'));
-        $this->assertEquals(2000, (new NumberSanitizers())->float()->__invoke('2000€'));
-        $this->assertEquals(2000.22, (new NumberSanitizers())->float()->__invoke('2000.22'));
-        $this->assertEquals(2000.22, (new NumberSanitizers())->float()->__invoke('2000.22€'));
-        $this->assertEquals(2000.22, (new NumberSanitizers())->float()->__invoke('2,000.22€'));
+        /*
+         */
+        $this->assertEquals(2000, (string) (new NumberSanitizers())->float()->__invoke('2000'));
+        $this->assertEquals(2000, (string) (new NumberSanitizers())->float()->__invoke('2000€'));
+        $this->assertEquals(2000.22, (string) (new NumberSanitizers())->float()->__invoke('2000.22'));
+        $this->assertEquals(2000.22, (string) (new NumberSanitizers())->float()->__invoke('2000.22€'));
+        $this->assertEquals(2000.22, (string) (new NumberSanitizers())->float()->__invoke('2,000.22€'));
+        $this->assertEquals("2.000,22", (string) (new NumberSanitizers())->float(true)->__invoke('2.000,22€'));
+        $this->assertEquals("2,000.22", (string) (new NumberSanitizers())->float(true)->__invoke('2,000.22€'));
+        $this->assertEquals("2.000.000,22", (string) (new NumberSanitizers())->float(true)->__invoke('2.000.000,22€'));
+        $this->assertEquals("2,000,000.22", (string) (new NumberSanitizers())->float(true)->__invoke('2,000,000.22€'));
+        $this->assertEquals(2000.22, (string) (new NumberSanitizers())->float()->__invoke('2000,22'));
+        $this->assertEquals(2000.22, (string) (new NumberSanitizers())->float()->__invoke('2000,22€'));
+        $this->assertEquals(2000000.22, (string) (new NumberSanitizers())->float()->__invoke('2.000.000,22€'));
+        $this->assertEquals(2000.22, (string) (new NumberSanitizers())->float()->__invoke('2.000,22€'));
     }
 
     public function testFloatSanitizerTrue()
     {
         $sanitizer = (new NumberSanitizers())->float();
-        $this->assertEquals(2000, $sanitizer('2000'));
-        $this->assertEquals(2000, $sanitizer('2000€'));
-        $this->assertEquals(2000.22, $sanitizer('2000.22'));
-        $this->assertEquals(2000.22, $sanitizer('2000.22€'));
-        $this->assertEquals(2000.22, $sanitizer('2,000.22€'));
+        $this->assertEquals(2000, (string) $sanitizer('2000'));
+        $this->assertEquals(2000, (string) $sanitizer('2000€'));
+        $this->assertEquals(2000.22, (string) $sanitizer('2000.22'));
+        $this->assertEquals(2000.22, (string) $sanitizer('2000.22€'));
+        $this->assertEquals(2000.22, (string) $sanitizer('2000,22'));
+        $this->assertEquals(2000.22, (string) $sanitizer('2000,22€'));
+        $this->assertEquals(2000.22, (string) $sanitizer('2,000.22€'));
     }
     public function testExtractInteger()
     {
-        $sanitizer = (new NumberSanitizers())->integer();
-        $this->assertEquals(123456789, $sanitizer('123456789N'));
-        $this->assertEquals(123456789, $sanitizer('Z123456789N'));
-        $this->assertEquals(123456789, $sanitizer('Z123456789'));
+        $sanitizer = (new NumberSanitizers())->float();
+        $this->assertEquals(123456789, (string) $sanitizer('123456789N'));
+        $this->assertEquals(123456789, (string) $sanitizer('Z123456789N'));
+        $this->assertEquals(123456789, (string) $sanitizer('Z123456789'));
     }
-        
+
 }
